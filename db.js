0d0774f56f90199b2a74a7de4ed2fd167c2a65bb
ruchi <ruchi@Linux-5.11.0-25>
2022-08-05 23:17:09 +0530

    05-08-2022

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:  'newpassword', 
-    database: 'online_exam'
-});
+// module.exports = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password:  'newpassword', 
+//     database: 'online_exam'
+// });
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
-const express = require('express');
+// const express = require('express');
 
-const router = express.Router();
+// const router = express.Router();
 
-const db = require('../db');
+// const db = require('../db');
 
-router.get('/allusers', async(req, res) => {
-    try {
-        const response = await db.promise().query('SELECT * FROM usersinfo');
-        console.log(response[0]);
-        res.status(200).json(response[0]);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-})
+// router.get('/allusers', async(req, res) => {
+//     try {
+//         const response = await db.promise().query('SELECT * FROM usersinfo');
+//         console.log(response[0]);
+//         res.status(200).json(response[0]);
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// })
 
 
-module.exports = router;
+// module.exports = router;
